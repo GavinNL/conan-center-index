0a50760fb412d29186eac4ad909680931e72bd46
Gavin Lobo <gavin.n.lobo@gmail.com>
2020-02-27 17:59:53 -0500

    Updated SPIRV-heaaders to match branch "spirv-headers"

--- recipes/spirv-headers/all/conanfile.py
+++ recipes/spirv-headers/all/conanfile.py
@@ -6,14 +6,13 @@ class SpirvheadersConan(ConanFile):
     name = "spirv-headers"
     homepage = "https://github.com/KhronosGroup/SPIRV-Headers"
     description = "SPIRV-Headers"
-    topics = ("conan", "sprv","vulkan")
+    topics = ("conan", "spirv", "spirv-v", "vulkan", "opengl", "opencl", "khronos")
     url = "https://github.com/conan-io/conan-center-index"
     settings = "os", "compiler", "arch", "build_type"
 
-    license = "BSD"
+    license = "MIT-KhronosGroup"
 
-    def requirements(self):
-        pass
+    _cmake = None
 
     @property
     def _source_subfolder(self):
@@ -25,10 +24,13 @@ class SpirvheadersConan(ConanFile):
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["SPIRV_HEADERS_SKIP_EXAMPLES"] = True
-        cmake.configure(source_folder=self._source_subfolder)
-        return cmake
+        if self._cmake:
+            return self._cmake
+
+        self._cmake = CMake(self)
+        self._cmake.definitions["SPIRV_HEADERS_SKIP_EXAMPLES"] = True
+        self._cmake.configure(source_folder=self._source_subfolder)
+        return self._cmake
 
     def build(self):
         cmake = self._configure_cmake()
@@ -36,12 +38,12 @@ class SpirvheadersConan(ConanFile):
 
     def package(self):
         self.copy(pattern="LICENSE*", dst="licenses", src=self._source_subfolder)
+
         cmake = self._configure_cmake()
         cmake.install()
 
         # Error KB-H016, complaining that Cmake config files are found
         tools.rmdir(os.path.join(self.package_folder, "lib"))
-
 
     def package_id(self):
         self.info.header_only()
